efa3d8b02a4b227bedb33de28ce96b21eb94411b
rdgrv <doraagnesvarga@gmail.com>
2018-11-16 20:52:24 +0100

    created create html skeleton

---
+++ src/main/java/com/greenfoxacademy/devwars/controllers/CharacterController.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.devwars.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CharacterController {
+
+  @GetMapping("/create")
+  public String getCreate(){
+    return "create";
+  }
+
+  @PostMapping("/create")
+  public String postCreate(){
+    return "redirect:/";
+  }
+}
