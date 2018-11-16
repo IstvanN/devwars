e7010466526b41a57ad2c94191da1c5146f1238c
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-11-16 22:18:10 +0100

    feat(Lobby): create FIGHT button and the post endpoint for it to enter the arena

--- src/main/java/com/greenfoxacademy/devwars/controllers/LobbyController.java
+++ src/main/java/com/greenfoxacademy/devwars/controllers/LobbyController.java
@@ -2,12 +2,21 @@ package com.greenfoxacademy.devwars.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LobbyController {
 
+
   @GetMapping("/lobby")
   public String showLobby() {
     return "lobby";
+  }
+
+  @PostMapping("/getCharacters")
+  public String getCharacters(@PathVariable("characterId") long characterId) {
+
+    return "redirect:/arena";
   }
 }
