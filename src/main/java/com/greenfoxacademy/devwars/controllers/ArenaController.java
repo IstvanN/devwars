255c9cd0a8fc3c8c7375844206e5afdf3785e965
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-11-16 21:56:00 +0100

    feat(welcomePage): create welcome.html

--- src/main/java/com/greenfoxacademy/devwars/controllers/ArenaController.java
+++ src/main/java/com/greenfoxacademy/devwars/controllers/ArenaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ArenaController {
 
-  @GetMapping("/")
+  @GetMapping("/arena")
   public String arena(){
     return "arena";
   }
