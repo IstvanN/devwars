b8d1c5682b7095a860ae89b9d418c278bd33b59f
Konock <bkonock@gmail.com>
2018-11-16 15:47:57 +0100

    initialize Character class with fields

--- src/main/java/com/greenfoxacademy/devwars/models/characterlogic/Character.java
+++ src/main/java/com/greenfoxacademy/devwars/models/characterlogic/Character.java
@@ -1,4 +1,32 @@
 package com.greenfoxacademy.devwars.models.characterlogic;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private long id;
+
+    private String name;
+    private int age;
+    private String os;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private League league;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    private Set<Competence> competenceList = new HashSet<>();
 }
