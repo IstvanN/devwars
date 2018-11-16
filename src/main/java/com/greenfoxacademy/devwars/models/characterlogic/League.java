d358395cd9b626fbc94c03e2768c3bb452588b48
Konock <bkonock@gmail.com>
2018-11-16 15:48:11 +0100

    initialize League class with fields

--- src/main/java/com/greenfoxacademy/devwars/models/characterlogic/League.java
+++ src/main/java/com/greenfoxacademy/devwars/models/characterlogic/League.java
@@ -1,4 +1,29 @@
 package com.greenfoxacademy.devwars.models.characterlogic;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class League {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String imgSource;
+
+    public League(String name) {
+        this.name = name;
+    }
 }
